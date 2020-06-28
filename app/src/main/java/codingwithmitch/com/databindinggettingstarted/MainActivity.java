da07995052188887bca375e6b781da16160f4f0e
urkeev14 <uros.veljkovic1996@gmail.com>
2020-06-28 16:06:49 +0200

    DataBinding implemented

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -1,13 +1,45 @@
 package codingwithmitch.com.databindinggettingstarted;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import codingwithmitch.com.databindinggettingstarted.databinding.ActivityMainBinding;
+import codingwithmitch.com.databindinggettingstarted.models.Product;
+import codingwithmitch.com.databindinggettingstarted.util.Products;
+
+public class MainActivity extends AppCompatActivity implements IMainActivity{
+
+    //data binding
+    ActivityMainBinding activityMainBinding;
+
+    //vars
+    private Product product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Products products = new Products();
+        product = products.PRODUCTS[0];
+
+        activityMainBinding.setProduct(product);
+        activityMainBinding.setQuantity(1);
+        activityMainBinding.setIMainActivity((IMainActivity)this);
+    }
+
+
+    @Override
+    public void onBtnQuantity() {
+        ChooseQuantityDialog dialog = new ChooseQuantityDialog();
+        dialog.show(getSupportFragmentManager(), getString(R.string.dialog_choose_quantity));
+    }
+
+    @Override
+    public void setQuantity(int quantity) {
+        activityMainBinding.setQuantity(quantity);
     }
 }
